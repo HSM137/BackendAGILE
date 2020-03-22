156312c2312e2cddc68431c7fb98b8f9e4a85fbc
faressoltani <43313698+faressoltani55@users.noreply.github.com>
2020-03-22 20:27:07 +0100

    first commit

---
+++ libs/signup/src/lib/singup/singup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'importili-singup',
+  templateUrl: './singup.component.html',
+  styleUrls: ['./singup.component.css']
+})
+export class SingupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
